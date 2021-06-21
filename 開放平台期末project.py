cc935f63a00be315e913e022341c401f8f103d50
loli-000 <51862517+loli-000@users.noreply.github.com>
2021-06-22 00:26:48 +0800

    Update 開放平台期末project.py

--- 開放平台期末project.py
+++ 開放平台期末project.py
@@ -26,7 +26,42 @@ def get(city):
         print("------------------------------------------------------")
         #print(j["records"]["location"][0]["weatherElement"][1]["time"][i]["parameter"]["parameterName"])
 
+def get2(waterbar):    
+    token = 'CWB-10872B0D-A923-47C9-9517-BC195576D0DB' #憑證
+    url = 'https://fhy.wra.gov.tw/WraApi/v1/Reservoir/Station?$select=StationNo%20%2CBasinName%2CStationName%20'
+    url2='https://fhy.wra.gov.tw/WraApi/v1/Reservoir/RealTimeInfo?$select=Time%2CWaterHeight%20%2CPercentageOfStorage%20%2CStationNo%20'
+    Data = requests.get(url)#對api發出請求
+    Data2= requests.get(url2)
+    #print(Data2.text)
+    j = json.loads(Data.text)#對json資料進行操作
+    j2 = json.loads(Data2.text)
+    index=0
+    index2=0
+    for i in j:
+        if waterbar==j[index]["StationName"]:
+            print(j[index]["StationNo"])
+            print(j[index]["StationName"])
+            print(j[index]["BasinName"])
+            break
+        index+=1
+    for i in j2:
+        if j[index]["StationNo"]==j2[index2]["StationNo"]:            
+            print("水情時間: ",j2[index2]["Time"])
+            print("水位高度: ",j2[index2]["WaterHeight"])
+            print("蓄水百分比: ",j2[index2]["PercentageOfStorage"])
+            if int(j2[index2]["PercentageOfStorage"])<=25:
+                print("-------------")
+                print("你家沒水囉~")
+                print("-------------")
+            break
+        index2+=1
+    
 print("輸入查詢縣市: ")
 location = input()
 print("=====================")
 get(location)
+print("=====================")
+print("輸入水庫名稱:　")
+waterbar = input()
+print("------------------------------------------------------")
+get2(waterbar)
